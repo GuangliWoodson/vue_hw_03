ae5450d883d17e4810db4eb13c64ec0a06425516
GuangliWoodson <guangliwoodson@outlook.com>
2022-07-22 16:11:25 +0800

    done

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,17 @@
-<!--  -->
 <template>
-<div class=''>
-	<myButtons type="danger" title="danger button" @click1="fn" @click="an">111</myButtons>
-	<myButtons type="warning" title="warning button"></myButtons>
-	<myButtons type="success" title="success button"></myButtons>
+<div>
+	<h1>{{sum(666)}}</h1>
 </div>
 </template>
 
 <script>
-//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
-//例如：import 《组件名称》 from '《组件路径》';
-import myButtons from './components/colorButtons.vue'
-
-export default {
-//import引入的组件需要注入到对象中才能使用
-components: {
-	myButtons
-},
-data() {
-//这里存放数据
-return {
-
-};
-},
-//监听属性 类似于data概念
-computed: {},
-//监控data中的数据变化
-watch: {},
-//方法集合
-methods: {
-	fn() {
-		console.log('233333');
-	},
-	an() {
-		console.log('111');
+	export default {
+		computed: {
+			sum () {
+				return function (n) {
+					return console.log('我是sum的参数' + n)
+				}
+			}
+		}
 	}
-},
-//生命周期 - 创建完成（可以访问当前this实例）
-created() {
-
-},
-//生命周期 - 挂载完成（可以访问DOM元素）
-mounted() {
-
-},
-beforeCreate() {}, //生命周期 - 创建之前
-beforeMount() {}, //生命周期 - 挂载之前
-beforeUpdate() {}, //生命周期 - 更新之前
-updated() {}, //生命周期 - 更新之后
-beforeDestroy() {}, //生命周期 - 销毁之前
-destroyed() {}, //生命周期 - 销毁完成
-activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
-}
 </script>
-<style scoped>
-
-</style>
